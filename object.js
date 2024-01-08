1814bcde752dafb23d0066990b98f7343684b3d2
Eric Mogendi <mogendieric19@gmail.com>
2024-01-08 16:48:28 +0300

    Method chaining in classes

--- object.js
+++ object.js
@@ -1,18 +1,23 @@
 class User {
-    constructor (email, name) {
+    constructor(email, name) {
         this.email = email;
         this.name = name;
+        this.score = 0;
     }
     login() {
         console.log(this.email, 'just logged in');
+        return this;
     }
     logout() {
         console.log(this.email, 'just logged out');
+        return this;
+    }
+    updateSCore() {
+        this.score++;
+        console.log(this.email, 'score is now', this.score)
+        return this;
     }
 }
 const UserOne = new User('mogendieric19@gmail.com', 'Eric')
 const Usertwo = new User('mogendicedric1@gmail.com', 'Cedric')
-console.log(UserOne)
-console.log(Usertwo)
-UserOne.login()
-UserOne.logout()
+UserOne.login().updateSCore().updateSCore().logout()
